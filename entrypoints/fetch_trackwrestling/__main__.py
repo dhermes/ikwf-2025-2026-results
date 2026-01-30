1af31f40546750c90a25b00f02ebdad26909de8d
Danny Hermes <daniel.j.hermes@gmail.com>
2026-01-30 08:57:07 -0600

    Actually integrating the data in and fixing the script.

--- entrypoints/fetch_trackwrestling/__main__.py
+++ entrypoints/fetch_trackwrestling/__main__.py
@@ -1,4 +1,3 @@
-import json
 import pathlib
 
 import bracket_util
@@ -6,6 +5,50 @@ import trackwrestling
 
 _HERE = pathlib.Path(__file__).resolve().parent
 _ROOT = _HERE.parent.parent
+
+
+def _fetch_event_rounds(
+    path: pathlib.Path, event: bracket_util.Event
+) -> bracket_util.FetchedEvent:
+    if path.exists():
+        with open(path, "rb") as file_obj:
+            as_json = file_obj.read()
+
+        return bracket_util.FetchedEvent.model_validate_json(as_json)
+
+    print(f"Fetching rounds for: {event.name} ...")
+    rounds_html = trackwrestling.fetch_tournament_rounds(event)
+
+    if not rounds_html:
+        raise RuntimeError("Tournament has no rounds", event.name)
+
+    fetched_event = bracket_util.FetchedEvent(
+        name=event.name,
+        source="trackwrestling",
+        start_date=event.start_date,
+        end_date=event.end_date,
+        match_html=rounds_html,
+        weights_html={},
+    )
+    return fetched_event
+
+
+def _fetch_event_weights(
+    fetched_event: bracket_util.FetchedEvent, event: bracket_util.Event
+) -> bracket_util.FetchedEvent | None:
+    if fetched_event.weights_html:
+        print(f"Skipping: {event.name} ...")
+        return None
+
+    print(f"Fetching weights for: {event.name} ...")
+
+    weights_html = trackwrestling.fetch_tournament_weights(event)
+
+    if not weights_html:
+        raise RuntimeError("Tournament has no weights", event.name)
+
+    fetched_event.weights_html = weights_html
+    return fetched_event
 
 
 def main() -> None:
@@ -17,35 +60,15 @@ def main() -> None:
         stem = bracket_util.to_kebab_case(name)
         filename = f"{stem}.json"
         path = parent_dir / filename
-        path2 = parent_dir / f"wo-{filename}"
-        if path2.exists():
-            print(f"Skipping: {name} ...")
-            continue
 
         event = bracket_util.Event(name=name, start_date=None, end_date=date_str)
+        fetched_event = _fetch_event_rounds(path, event)
+        fetched_event = _fetch_event_weights(fetched_event, event)
 
-        print(f"Fetching: {name} ...")
-
-        weights_html = trackwrestling.fetch_tournament_weights(event)
-        with open(path2, "w") as file_obj:
-            json.dump(weights_html, file_obj)
-
-        if 2 + 2 == 3 + 1:
+        if fetched_event is None:
             continue
 
-        rounds_html = trackwrestling.fetch_tournament_rounds(event)
-
-        if not rounds_html:
-            raise RuntimeError("Tournament has no rounds", name)
-
-        to_serialize = bracket_util.FetchedEvent(
-            name=event.name,
-            source="trackwrestling",
-            start_date=event.start_date,
-            end_date=event.end_date,
-            match_html=rounds_html,
-        )
-        as_json = to_serialize.model_dump_json(indent=2)
+        as_json = fetched_event.model_dump_json(indent=2)
         with open(path, "w") as file_obj:
             file_obj.write(as_json)
             file_obj.write("\n")
